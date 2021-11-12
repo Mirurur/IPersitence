30c991bfba0ce0d723c1b9a5a3cf717096af930e
yeyu <yeyu@edanding.com>
2021-11-12 17:56:53 +0800

    sqlSession添加一级缓存

--- src/main/java/com/amateur/sqlsession/DefaultSqlSessionFactory.java
+++ src/main/java/com/amateur/sqlsession/DefaultSqlSessionFactory.java
@@ -8,7 +8,7 @@ import com.amateur.pojo.Configuration;
  */
 public class DefaultSqlSessionFactory implements SqlSessionFactory{
 
-    private Configuration configuration;
+    private final Configuration configuration;
 
     public DefaultSqlSessionFactory(Configuration configuration){
         this.configuration = configuration;
